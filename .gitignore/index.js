b541eb01ae2bb836dc389202dc84d5ecdfecf967
oOTeamCocOo <46351483+oOTeamCocOo@users.noreply.github.com>
2019-04-24 11:16:14 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -6,7 +6,7 @@ var prefix = "*";
 
 client.on('ready', () => {
     console.log('logged in as ' + client.user.tag)
-    client.user.setActivity('fromtentain',{type: 'WATCHING'})
+    client.user.setActivity('la sécurité de ton serveur !',{type: 'WATCHING'})
 });
 
 client.login(process.env.TOKEN)
